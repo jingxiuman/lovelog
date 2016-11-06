0e9c118ea82960842083f0eb919616cb4ece38fa
knowthis <admin@zhouxianbao.cn>
2016-11-06 23:03:22 +0800

    change host

--- app/host.js
+++ app/host.js
@@ -2,15 +2,15 @@
 define([], function (argument) {
 	var urlObj = {
 		dev: {
-			api: 'http://lovelog.dev.zhouxianbao.cn/',
+			api: '//lovelog.dev.zhouxianbao.cn/',
             imgUrl:'http://7xlabr.com1.z0.glb.clouddn.com/',
 		},
 		test: {
-			api: 'http://lovelog.dev.zhouxianbao.cn/',
+			api: '//lovelog.dev.zhouxianbao.cn/',
             imgUrl:'http://7xlabr.com1.z0.glb.clouddn.com/',
 		},
 		release: {
-			api: 'http://lovelog.zhouxianbao.cn/',
+			api: '//lovelog.zhouxianbao.cn/',
             imgUrl:'http://7xlabr.com1.z0.glb.clouddn.com/',
 		}
 	};
